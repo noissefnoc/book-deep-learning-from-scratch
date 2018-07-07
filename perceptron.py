84f9b37aceb1a7ff833a1c2f599a6ed77d8d8d4e
noissefnoc <noissefnoc@gmail.com>
2018-07-07 17:10:50 +0900

    add nand and or gate

--- perceptron.py
+++ perceptron.py
@@ -18,3 +18,37 @@ def AND(x1, x2):
         return 0
     else:
         return 1
+
+
+def NAND(x1, x2):
+    """
+
+    :param x1:
+    :param x2:
+    :return:
+    """
+    x = np.array([x1, x2])
+    w = np.array([-0.7, -0.7])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1, x2):
+    """
+
+    :param x1:
+    :param x2:
+    :return:
+    """
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.array(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
