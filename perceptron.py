20704a7d45991b33c6423f66ec10df11894f4391
noissefnoc <noissefnoc@gmail.com>
2018-07-07 16:37:51 +0900

    complete implement 2.3.3 and gate

--- perceptron.py
+++ perceptron.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
+import numpy as np
+
 
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1 * w1 + x2 * w2
-    if tmp <= theta:
-        return 0
-    elif tmp > theta:
-        return 1
+    """
 
+    :param x1:
+    :param x2:
+    :return:
+    """
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
